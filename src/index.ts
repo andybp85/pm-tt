3b2ef5f311050acc169f7f2392e6e036f167c13d
Andrew Stanish <andrew.stanish@nbcuni.com>
2017-10-17 10:25:28 -0400

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,14 @@ import {EditorState} from 'prosemirror-state'
 import {EditorView} from 'prosemirror-view'
 import {undo, redo, history} from 'prosemirror-history'
 import {keymap} from 'prosemirror-keymap'
-import {
-  baseKeymap,
-  chainCommands,
-  newlineInCode,
-  createParagraphNear,
-  liftEmptyBlock,
-  splitBlock
-} from 'prosemirror-commands'
+import {baseKeymap, chainCommands, newlineInCode, createParagraphNear, liftEmptyBlock,
+  splitBlock} from 'prosemirror-commands'
 
-import {
-  schema,
-  splitListItem,
-  liftListItem,
-  sinkListItem
-} from './schema'
+import {schema, splitListItem, liftListItem, sinkListItem} from './schema'
 import {menuPlugin} from './menu'
-import {
-  subHeader,
-  bold,
-  italics,
-  link,
-  bullet_list,
-  ordered_list,
-  blockquote,
-  sup,
-  sub,
-  specialCharacters
-} from './menuItems'
 import {tooltipPlugin} from './tooltip'
+import {subHeader, bold, italics, link, bullet_list, ordered_list, blockquote, sup, sub,
+  specialCharacters} from './menuItems'
 
 export const menu = menuPlugin([
   subHeader(),
